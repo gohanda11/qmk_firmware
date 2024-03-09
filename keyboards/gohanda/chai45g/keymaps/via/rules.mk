f25b3ed3f4913e730fe165d2ce6b45ca2ad44107
gohanda11 <123556676+gohanda11@users.noreply.github.com>
2024-03-09 15:14:36 +0900

    Delete keyboards/gohanda/chai45g directory

--- keyboards/gohanda/chai45g/keymaps/via/rules.mk
+++
@@ -1,4 +0,0 @@
-VIA_ENABLE = yes
-OS_DETECTION_ENABLE = yes
-SRC += add_keycodes.c
-SRC += add_trackball.c
--- keyboards/gohanda/chai45g/mcuconf.h
+++
@@ -1,8 +0,0 @@
-// Copyright 2018-2022 Nick Brassel (@tzarc)
-// SPDX-License-Identifier: GPL-3.0-or-later
-#pragma once
-
-#include_next <mcuconf.h>
-
-#undef RP_SPI_USE_SPI0
-#define RP_SPI_USE_SPI0 TRUE
