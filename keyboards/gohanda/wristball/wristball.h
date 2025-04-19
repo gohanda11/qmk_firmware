635b7d66386ad556383365de79377bdecaa13a5d
gohanda <123556676+gohanda11@users.noreply.github.com>
2025-04-19 16:44:47 +0900

    Update wristball.h

--- keyboards/gohanda/wristball/wristball.h
+++ keyboards/gohanda/wristball/wristball.h
@@ -14,68 +14,69 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
-#pragma once
-
-#include "quantum.h"
-
-#define LAYOUT( \
-    A00, A01, A10, A11 \
-   ) \
-  { \
-    { A00, A01 }, \
-    { A10, A11 }  \
-  }
-
-typedef union {
-    uint32_t raw;
-    struct {
-        uint8_t cpi_idx;
-        uint8_t scrl_div;
-        uint8_t rotation_angle;
-        int8_t scrl_inv;
-        bool scrl_mode;
-        report_mouse_t last_mouse;
-    };
-} cocot_config_t;
-
-extern cocot_config_t cocot_config;
-
-enum cocot_keycodes {
-
-    COCOT_SAFE_RANGE = SAFE_RANGE,
-    CPI_SW,
-    SCRL_SW,
-    ROT_R15,
-    ROT_L15,
-    SCRL_MO,
-    SCRL_TO,
-    SCRL_IN,
-};
-
-#define CPI_SW QK_KB_0
-#define SCRL_SW QK_KB_1
-#define ROT_R15 QK_KB_2
-#define ROT_L15 QK_KB_3
-#define SCRL_MO QK_KB_4
-#define SCRL_TO QK_KB_5
-#define SCRL_IN QK_KB_6
-
-
-
-bool encoder_update_user(uint8_t index, bool clockwise);
-bool encoder_update_kb(uint8_t index, bool clockwise);
-
-bool cocot_get_scroll_mode(void);
-void cocot_set_scroll_mode(bool mode);
-
-
-void enable_click_layer(void);
-void disable_click_layer(void);
-int16_t my_abs(int16_t num);
-int16_t mmouse_move_y_sign(int16_t num);
-bool is_clickable_mode(void);
-
-
-void render_logo(void);
-void oled_write_layer_state(void);
+ #pragma once
+ #include "quantum.h"
+ 
+ #define LAYOUT( \
+     A00, A01, A10, A11 \
+    ) \
+   { \
+     { A00, A01 }, \
+     { A10, A11 }  \
+   }
+ 
+ typedef union {
+     uint32_t raw;
+     struct {
+         uint8_t cpi_idx;
+         uint8_t scrl_div;
+         uint8_t rotation_angle;
+         int8_t scrl_inv;
+         bool scrl_mode;
+         report_mouse_t last_mouse;
+         bool rotate_270; // 270度回転か180度回転かを保存するフラグ
+     };
+ } cocot_config_t;
+ 
+ extern cocot_config_t cocot_config;
+ 
+ enum cocot_keycodes {
+ 
+     COCOT_SAFE_RANGE = SAFE_RANGE,
+     CPI_SW,
+     SCRL_SW,
+     ROT_R15,
+     ROT_L15,
+     SCRL_MO,
+     SCRL_TO,
+     SCRL_IN,
+     ROT_SW, // 回転角度を切り替えるキーコード
+ };
+ 
+ #define CPI_SW QK_KB_0
+ #define SCRL_SW QK_KB_1
+ #define ROT_R15 QK_KB_2
+ #define ROT_L15 QK_KB_3
+ #define SCRL_MO QK_KB_4
+ #define SCRL_TO QK_KB_5
+ #define SCRL_IN QK_KB_6
+ #define ROT_SW QK_KB_7 // 回転角度切り替えのマクロ定義
+ 
+ 
+ 
+ bool encoder_update_user(uint8_t index, bool clockwise);
+ bool encoder_update_kb(uint8_t index, bool clockwise);
+ 
+ bool cocot_get_scroll_mode(void);
+ void cocot_set_scroll_mode(bool mode);
+ 
+ 
+ void enable_click_layer(void);
+ void disable_click_layer(void);
+ int16_t my_abs(int16_t num);
+ int16_t mmouse_move_y_sign(int16_t num);
+ bool is_clickable_mode(void);
+ 
+ 
+ void render_logo(void);
+ void oled_write_layer_state(void);
